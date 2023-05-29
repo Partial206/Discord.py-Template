31b47eef96051017074d66852657adcbb7c6c691
Partial <133568898+Partial206@users.noreply.github.com>
2023-05-29 22:17:15 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+from utils import *
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guild_messages = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!a", intents=intents)
+
+bot.run('token')
